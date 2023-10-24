c70ef7489313a353be6321efc4ad445ee4673193
Enrico <enrico.rossini.uk@live.com>
2023-10-24 16:13:43 +0100

    Rename Identity Table names

--- BlazorIdentity/BlazorIdentity/BlazorIdentity/Data/ApplicationDbContext.cs
+++ BlazorIdentity/BlazorIdentity/BlazorIdentity/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,5 +6,38 @@ namespace BlazorIdentity.Data
 {
 	public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 	{
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+			builder.HasDefaultSchema("Identity");
+			builder.Entity<IdentityUser>(entity =>
+			{
+				entity.ToTable(name: "User");
+			});
+			builder.Entity<IdentityRole>(entity =>
+			{
+				entity.ToTable(name: "Role");
+			});
+			builder.Entity<IdentityUserRole<string>>(entity =>
+			{
+				entity.ToTable("UserRoles");
+			});
+			builder.Entity<IdentityUserClaim<string>>(entity =>
+			{
+				entity.ToTable("UserClaims");
+			});
+			builder.Entity<IdentityUserLogin<string>>(entity =>
+			{
+				entity.ToTable("UserLogins");
+			});
+			builder.Entity<IdentityRoleClaim<string>>(entity =>
+			{
+				entity.ToTable("RoleClaims");
+			});
+			builder.Entity<IdentityUserToken<string>>(entity =>
+			{
+				entity.ToTable("UserTokens");
+			});
+		}
 	}
 }
